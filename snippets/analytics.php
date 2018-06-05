3388821272d4c751bea34a676f27672e18aaa456
Volker Otto <volker.otto@digital-masters.de>
2018-06-05 23:13:33 +0200

    added google tag manager facebook + optout cookie new google analytics tag gtag.js

--- snippets/analytics.php
+++ snippets/analytics.php
@@ -7,6 +7,14 @@
     $gaId   = c::get('analytics.google.id'          , false);
     $gaAnon = c::get('analytics.google.anonymizeip' , true);
 
+    // Google Tag Manager
+    $gTag   = c::get('analytics.googletm'             , false);
+    $gTagId = c::get('analytics.googletm.id'          , false);
+
+    // Facebook
+    $fb     = c::get('analytics.facebook', false);
+    $fbId   = c::get('analytics.facebook.id', false);
+
     // Piwik
     $piwik    = c::get('analytics.piwik'     , false);
     $piwikUrl = c::get('analytics.piwik.url' , false);
@@ -44,28 +52,31 @@
     }
 ?>
 <?php if ($ga && $gaId): ?>
-    <!-- Google Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $gaId; ?>"></script>
     <script>
-        var gaProperty = '<?php echo $gaId; ?>';
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-        window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-        ga('create', gaProperty, 'auto');
-        <?php if ($gaAnon): ?>
-            ga('set', 'anonymizeIp', true);
-        <?php endif; ?>
-        ga('send','pageview');
+    <?php if ($gaAnon): ?>
+        gtag('config', '<?php echo $gaId; ?>', { 'anonymize_ip': true });
+    <?php else: ?>
+        gtag('config', '<?php echo $gaId; ?>');
+    <?php endif; ?>
 
-        var disableStr = 'ga-disable-' + gaProperty;
-        if (document.cookie.indexOf(disableStr + '=true') > -1) {
-            window[disableStr] = true;
-        }
-        function gaOptout() {
-            document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-            window[disableStr] = true;
-            alert('Google Analytics Opt Out successfull!');
-        }
+    var gaProperty = '<?php echo $gaId; ?>';
+
+    var disableStr = 'ga-disable-' + gaProperty;
+    if (document.cookie.indexOf(disableStr + '=true') > -1) {
+        window[disableStr] = true;
+    }
+    function gaOptout() {
+        document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+        window[disableStr] = true;
+        alert('Google Analytics Opt Out successfull!');
+    }
     </script>
-    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     <!-- / Google Analytics -->
 <?php endif; ?>
 
@@ -114,4 +125,48 @@
     for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.__SV=1.2;b=e.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c=e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)}})(document,window.mixpanel||[]);
     mixpanel.init("<?php echo $mixToken?>");</script>
     <!-- / Mixpanel -->
+<?php endif; ?>
+
+<?php if ($fb && $fbId): ?>
+    <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+    var fbDisableStr = 'fb-disable-pixel';
+
+    if (document.cookie.indexOf(fbDisableStr + '=true') > -1) {
+        window[fbDisableStr] = true;
+    } else {
+        window[fbDisableStr] = false;
+        fbq('init', '<?=$fbId?>');
+        fbq('track', 'PageView');
+    }
+
+    function fbOptout() {
+        document.cookie = fbDisableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+        window[fbDisableStr] = true;
+        alert('Sie haben die Erfassung Ihrer Daten mittels des Facebook-Pixels auf unserer Webseite deaktiviert.');
+    }
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?=$fbId?>&ev=PageView&noscript=1"/></noscript>
+    <!-- / Facebook Pixel Code -->
+<?php endif; ?>
+
+<?php if ($gTag && $gTagId): ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','<?=$gTagId?>');</script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?=$gTagId?>"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 <?php endif; ?>
